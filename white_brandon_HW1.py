6dcd0e647d66a9f0d874eaace658b1905b10423b
Brandon-White-08 <bcwhitecb@gmail.com>
2019-08-26 21:24:00 -0500

    Update white_brandon_HW1.py

--- white_brandon_HW1.py
+++ white_brandon_HW1.py
@@ -2,46 +2,46 @@
 #**************************************************
 #Author:	Brandon White
 #Date:		08/26/2019
-#Desc:		This set of solve the problems given
-#			on HW1 of MAE 5010. 
+#Desc:		This set of functions to solve the problems
+#			given on HW1 of MAE 5010. 
 #**************************************************
 
 def Euler3212EP(angles, radians = False, rounding = True):
-	#Expects degrees but can accept radians with flag set 
-	
+	#Expects degrees but can accept radians with flag set
+
 	import math
-	
+
 	#convert to radians for math
 	if not radians:
 		for i in range(3):
 			angles[i] = angles[i] * (math.pi/180)
-	
+
 	[psi, theta, phi] = angles
-	
+
 	e = [math.cos(psi/2)*math.cos(theta/2)*math.cos(phi/2) + math.sin(psi/2)*math.sin(theta/2)*math.sin(phi/2),
 			math.cos(psi/2)*math.cos(theta/2)*math.sin(phi/2) - math.sin(psi/2)*math.sin(theta/2)*math.cos(phi/2),
 			math.cos(psi/2)*math.sin(theta/2)*math.cos(phi/2) + math.sin(psi/2)*math.cos(theta/2)*math.sin(phi/2),
 			math.sin(psi/2)*math.cos(theta/2)*math.cos(phi/2) - math.cos(psi/2)*math.sin(theta/2)*math.sin(phi/2)]
-	
+
 	if rounding:
 		for i in range(4):
 			e[i] = round(e[i],5)
-	
+
 	return e
 
 def EP2Euler321(e, rounding = True):
 	import math
-	
+
 	angles = [math.atan2(2 * (e[0]*e[1] + e[2]*e[3]), e[0]**2 + e[3]**2 - e[1]**2 - e[2]**2),
 				math.asin(2 * (e[0]*e[2] - e[1]*e[3])),
 				math.atan2(2 * (e[0]*e[3] + e[2]*e[1]), e[0]**2 + e[1]**2 - e[2]**2 - e[3]**2)]
-	
+
 	#Convert to degrees
 	for i in range(3):
 		angles[i] = angles[i] * (180/math.pi)
 		if rounding:
 			angles[i] = round(angles[i],2)
-	
+
 	return angles #returns degrees
 
 def make_gamma(I):
@@ -60,14 +60,14 @@ def make_gamma(I):
 def pos_kin(psi, theta, phi):
 	#d/dt([p_n, p_e, p_d])
 	from numpy import matmul
-	
+
 	A1 = [[cos(theta)*cos(psi), sin(phi)*sin(theta)*sin(psi) - cos(phi)*cos(psi), cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi)],
 				[cos(theta)*sin(psi), sin(phi)*sin(theta)*sin(psi) + cos(phi)*cos(psi), cos(phi)*sin(theta)*sin(psi) - sin(phi)*cos(psi)],
 				[-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)]]
 	b1 = [u, v, w]
 	return matmul(A1, b1)
 
-def pos_dyn(u, v, w): 
+def pos_dyn(u, v, w):
 	#d/dt([u, v, w])
 	x_dot = [[r*v-q*w + Fx/m],
 			[p*w-r*u + Fy/m],
@@ -79,7 +79,7 @@ def rot_kin(e0, e1, e2, e3):
 	from numpy import matmul
 	A3 = [[0, -p/2, -q/2, -r/2],
 			[p/2, 0, r/2, -q/2],
-			[q/2, -r/2, 0, p/2], 
+			[q/2, -r/2, 0, p/2],
 			[r/2, q/2, -p/2, 0]]
 	b3 = [e0, e1, e2, e3]
 	return matmul(A3, b3)
@@ -94,40 +94,34 @@ def rot_dyn(Gamma):
 def derivatives(state, FM, MAV):
 	#state:	[p_n, p_e, p_d, u, v, w, e0, e1, e2, e3, p, q, r]
 	#FM:	[Fx, Fy, Fz, Ell, M, N]
-	#MAV:	MAV.I, MAV.m, MAV.gravity_needed 
-	
+	#MAV:	MAV.I, MAV.m, MAV.gravity_needed
+
 	from math import sin, cos
 	import EP2Euler321
-	
+
 	#Unpack state, FM, MAV.I
 	[p_n, p_e, p_d, u, v, w, e0, e1, e2, e3, p, q, r] = state
 	[Fx, Fy, Fz, L, M, N] = FM
 	[Ixz, Ix, Iy, Iz] = MAV.I
-	
+
 	#Add gravity if not included
-	if MAV.gravity_needed:
+if MAV.gravity_needed:
 		Fz = Fz - 32.2*MAV.m
-		
+
 	#Get angle measures
-	angles = EP2Euler321([e0, e1, e2, e3]) 
+	angles = EP2Euler321([e0, e1, e2, e3])
 	[psi, theta, phi] = angles
-	
+
 	#Get Xdot Terms
 	d_dt = [[],[],[]]
 	d_dt[0] = pos_kin(psi, theta, phi)
 	d_dt[1] = pos_dyn(u,v,w)
 	d_dt[2] = rot_kin(e0, e1, e2, e3)
 	d_dt[3] = rot_dyn(make_gamma(MAV.I))
-	
+
 	#Build One Vector of Xdot
 	for eqn_set in d_dt:
 		for dot in eqn_set:
 			xdot.append(dot)
-			
+
 	return xdot
-	
-	
-	
-	
-	
-	
