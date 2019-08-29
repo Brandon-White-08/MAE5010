c3acd29220f2cf3edfdd2eb37170cd94f8c0ada8
Brandon-White-08 <bcwhitecb@gmail.com>
2019-08-29 00:07:05 -0500

    Added Quaternian Plots

--- quick_run.py
+++ quick_run.py
@@ -5,4 +5,15 @@ my_mav = MAV()
 my_mav.state0
 my_mav.FMeq
 [t,s] = HW1.integrator(my_mav)
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+fig.suptitle('quaternions')
+ax1.plot(t,s[:,6])
+ax2.plot(t,s[:,7], 'tab:orange')
+ax3.plot(t,s[:,8], 'tab:green')
+ax4.plot(t,s[:,9], 'tab:red')
+for ax in fig.get_axes():
+    ax.label_outer()
+plt.show()
+
 state_viewer.open_GUI(t, s)
